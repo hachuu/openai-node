f479688b1b4381f1b9483cae4b0ce14a13ca1b5e
hayoung <hayoung.song@kt.com>
2023-04-24 17:59:02 +0900

    chat 작업 중

--- pages/api/findAnswer.js
+++ pages/api/findAnswer.js
@@ -5,6 +5,13 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const messagesHistory = [{role: 'assistant', content: `
+  스무고개 게임 시작!
+  절대 사용자에게 정답을 언급하면 안됩니다.
+  사용자가 질문할 때마다, 당신은 그에게 답변을 해주어야 합니다.
+  사용자가 정답을 언급하면 '정답입니다!'라고 답변해주세요.
+` }];
+
 export default async function (req, res) {
   if (!configuration.apiKey) {
     res.status(500).json({
@@ -19,51 +26,36 @@ export default async function (req, res) {
     const answer = req.body.correctAnswer;
     const text = req.body.text;
 
-    const prompt = 
-    `
-      스무고개 게임 시작!
-      정답은 ${answer}입니다.
-      사용자가 질문할 때마다, 당신은 그에게 답변을 해주어야 합니다.
-      사용자가 '${answer}'을 언급하면 '정답입니다!'라고 답변해주세요.
-      사용자가 질문하는 내용을 입력해주세요:
-
-      사용자: 이 동물은 포유류인가요?
-      AI: (포유류인 경우) 네, 포유류입니다. / (아닌 경우) 아니요, 포유류가 아닙니다.
-      사용자: ${text}
-      AI: 
-    `
+    
     // `
     //   스무고개 게임 정답은 ${answer}이고, 사용자는 답을 몰라.
     //   너는 ${answer}을 알려주면 안되고 사용자가 입력한 질문에 맞게 답변을 해줘.
     //   질문: ${text}
     //   AI:
     // `
+    if (messagesHistory.length === 1) {
+      messagesHistory.push({role: "assistant", content: `정답은 ${answer}입니다.`})
+    }
 
-    console.log('findAnswer prompt : ', prompt)
+    messagesHistory.push({role: "user", content: text})
+    
+    console.log('findAnswer messagesHistory : ', messagesHistory);
 
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt,
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
       max_tokens: 1000,
       temperature: 0.5,
-      // top_p: 1.0,
-      // presence_penalty: -1.0,
-      // frequency_penalty: -1.0,
-      // best_of: 2,
       stop: ['\n\n', '\n', 'AI:'],
-      // n: 1,
-      // stream: false,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      best_of: 1,
-      user: "curious-ai-hy",
+      messages: messagesHistory
     });
-    console.log('스무고개 질문에 대한 답 : ', completion.data);
+    console.log('스무고개 질문에 대한 답 : ', completion.data, completion.data.choices[0].message.content);
     
     //answer 여러개 있을 수 있음
     const pattern = new RegExp(answer, 'g');
     const removeAnswer = 'O'.repeat(answer.length);
-    const result = completion.data.choices[0].text.replace(pattern, removeAnswer);
+    const result = completion.data.choices[0].message.content?.replace(pattern, removeAnswer);
+    messagesHistory.push({role: "system", content: result})
+
     const success = result.includes('정답입니다!');
 
     res.status(200).json({ result, success });
@@ -82,16 +74,18 @@ export default async function (req, res) {
     }
   }
 }
-
-function generatePrompt(animal) {
-  const capitalizedAnimal =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
-}
+// model: "gpt-3.5-turbo",
+//       prompt,
+//       max_tokens: 1000,
+//       temperature: 0.5,
+//       // top_p: 1.0,
+//       // presence_penalty: -1.0,
+//       // frequency_penalty: -1.0,
+//       // best_of: 2,
+//       stop: ['\n\n', '\n', 'AI:'],
+//       // n: 1,
+//       // stream: false,
+//       presence_penalty: 0,
+//       frequency_penalty: 0,
+//       best_of: 1,
+//       user: "curious-ai-hy",
