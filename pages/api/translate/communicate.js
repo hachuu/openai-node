219a36e52aec21715741a7456e0173f28f1b1e34
hayoung <hayoung.song@kt.com>
2023-05-24 15:14:59 +0900

    res header 수정

--- pages/api/translate/communicate.js
+++ pages/api/translate/communicate.js
@@ -19,8 +19,8 @@ export default async function (req, res) {
     // console.log(req.body);
     console.log(req.body);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Origin', 'http://125.159.61.195:30011');
     // res.setHeader('Content-Type', 'application/json');
     // res.setHeader('Accept', '*/*');
