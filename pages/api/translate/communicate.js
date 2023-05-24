64c60c9a6b78dfe978125dc49bb59dd0a2f3a0e8
hayoung <hayoung.song@kt.com>
2023-05-24 14:54:19 +0900

    Access-Control-Allow-Origin 추가

--- pages/api/translate/communicate.js
+++ pages/api/translate/communicate.js
@@ -18,7 +18,7 @@ export default async function (req, res) {
   try {
     // console.log(req.body);
     console.log(req.body);
-    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     // res.setHeader('Origin', 'http://localhost:8080'),
