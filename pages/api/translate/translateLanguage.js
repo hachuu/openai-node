7f8386d3c32a2829ddb460c3d573e927279af6f2
hayoung <hayoung.song@kt.com>
2023-06-19 16:44:44 +0900

    req method 추가

--- pages/api/translate/translateLanguage.js
+++ pages/api/translate/translateLanguage.js
@@ -17,38 +17,49 @@ export default async function (req, res) {
 
   try {
 
-    const lang = req.body.targetLanguage || JSON.parse(req.body).targetLanguage ;
-    const text = req.body.text || JSON.parse(req.body).text ;
+    if (req.method === 'OPTIONS') {
+      // OPTIONS 요청에 대한 헤더 처리
+      res.status(200)
+        .setHeader('Access-Control-Allow-Origin', process.env.VUE_APP_TB_DOMAIN)
+        .setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        .setHeader('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept, crossDomain, withCredentials, SameSite, Set-Cookie')
+        .setHeader('Origin', process.env.VUE_APP_TB_DOMAIN)
+        .setHeader('Access-Control-Allow-Credentials', 'true');
+      res.end();
+    } else {
+      const lang = req.body.targetLanguage || JSON.parse(req.body).targetLanguage ;
+      const text = req.body.text || JSON.parse(req.body).text ;
 
-    const prompt = 
-    `
-    제 말을 ${lang}언어로 번역해 주세요.
-    번역할 문장: ${text}
-    번역:
-    `
+      const prompt = 
+      `
+      제 말을 ${lang}언어로 번역해 주세요.
+      번역할 문장: ${text}
+      번역:
+      `
 
-    console.log(prompt);
-    console.log(req.body)
+      console.log(prompt);
+      console.log(req.body)
 
-    const completion = await openai.createCompletion({
-      model: 'text-davinci-003',
-      prompt,
-      max_tokens: 1000,
-      temperature: 0.9,
-      // top_p: 1.0,
-      // presence_penalty: -1.0,
-      // frequency_penalty: -1.0,
-      // best_of: 2,
-      // stop: ['\n\n', '\n', 'Answer:'],
-      // n: 1,
-      // stream: false,
-      presence_penalty: 0,
-      frequency_penalty: 0,
-      best_of: 1,
-      // user: "curious-ai-hy",
-    });
-    const answer = completion.data.choices[0].text;
-    res.status(200).json({ result: answer });
+      const completion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt,
+        max_tokens: 1000,
+        temperature: 0.9,
+        // top_p: 1.0,
+        // presence_penalty: -1.0,
+        // frequency_penalty: -1.0,
+        // best_of: 2,
+        // stop: ['\n\n', '\n', 'Answer:'],
+        // n: 1,
+        // stream: false,
+        presence_penalty: 0,
+        frequency_penalty: 0,
+        best_of: 1,
+        // user: "curious-ai-hy",
+      });
+      const answer = completion.data.choices[0].text;
+      res.status(200).json({ result: answer });
+    }
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
